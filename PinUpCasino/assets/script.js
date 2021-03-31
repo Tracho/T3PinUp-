2a3e8c627c431ad474bf145ef3eb366f13ae9716
IgorShuma <Tracho2013@gmail.com>
2021-03-31 08:41:32 +0300

    FirstCommit

---
+++ PinUpCasino/assets/script.js
@@ -0,0 +1,6 @@
+let BurgerJs = document.querySelector("#BurgerJs"),
+    sidebarJs = document.querySelector("#sidebarJs");
+BurgerJs.addEventListener("click", () => {
+    BurgerJs.classList.toggle("burger_active");
+    sidebarJs.classList.toggle("sidebar--hidden");
+});
